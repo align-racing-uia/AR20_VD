b392e26318066676a080c6662648e96751e4641a
Iver Sørensen <1996@gmail.com>
2020-09-18 14:16:53 +0200

    test iver

--- Forsøk 2/test.m
+++
@@ -1,9 +0,0 @@
-%deg fra 2 vektorer 
-
-FOYV= [1 0 1]
-FNYV = [0 0 0]
-
-a=[0,1]
-b=[FOYV(1)-FNYV(1) , FOYV(3)-FNYV(3)]
-alpha = acos((a(1) * b(1) + a(2) * b(2)) / (sqrt((a(1)^2 + a(2)^2)) * sqrt((b(1)^2 + b(2)^2))))
-alpha = rad2deg(alpha)
