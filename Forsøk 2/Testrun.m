13d40bc87b2b8253915ec75fc442beaf81489d83
Iver Sørensen <1996@gmail.com>
2021-01-14 17:11:35 +0100

    Geometri fix

--- Forsøk 2/Testrun.m
+++ Forsøk 2/Testrun.m
@@ -9,7 +9,6 @@ Geometri
 %dynamikk script
 [RC_F]=RC_calculation(W_FOXV , W_FNXV , Wc_FV , Wb_F , Wd_F)
 [RC_B]=RC_calculation(W_BOXV , W_BNXV , Wc_BV , Wb_B , Wd_B)
-
 Ilustrasjon
 
 %Git test test
